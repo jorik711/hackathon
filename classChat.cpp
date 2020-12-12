9bc0439570f38f297639bd48b7c06218b2943bf8
Egor <711jorik@gmail.com>
2020-12-12 14:16:49 +0300

    Add Chat & Message classes

---
+++ classChat.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "classChat.h"
+
+Chat::Chat(std::vector<User> user, std::vector<std::string> message)
+{
+
+}
+Chat::void runChat()
+{
+
+}
+Chat::User registration()
+{
+
+}
+Chat::bool logInFunc(User user)
+{
+
+}
+Chat::void showUser()
+{
+
+} 
+Chat::~Chat()
+{
+
+}
---
+++ classMessage.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "classMessage.h"
+
+
+Message::Message()
+{
+
+}
+Message::Message sendMessage(User user, std::string message)
+{
+
+}
+Message::void readMessage(User user)
+{
+    
+}
+Message::void setSenduser(User user)
+{
+
+}
+Message::void setMuser(User user)
+{
+
+}
+Message::~Message()
+{
+    
+}
